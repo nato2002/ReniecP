da83bb7068c6b5e47c7710f8b139836407cda481
nato2002 <quisperenato03@gmail.com>
2023-06-16 23:12:51 -0500

    .

--- LfServiceReniecCliente/LogIn.aspx.designer.cs
+++ LfServiceReniecCliente/LogIn.aspx.designer.cs
@@ -114,15 +114,6 @@ namespace LfServiceReniecCliente
         protected global::System.Web.UI.WebControls.Panel Panel2;
 
         /// <summary>
-        /// Control Label4.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Label Label4;
-
-        /// <summary>
         /// Control btnLogin.
         /// </summary>
         /// <remarks>
